a3fbf218f666f2880e71ddd4a8dfc2d7d50fa484
Yaolin Ge <geyaolin@gmail.com>
2024-06-19 18:49:30 +0200

    update notes

--- Practice/DemoApp/RealTimePlot/obj/Debug/net8.0/RealTimePlot.AssemblyInfo.cs
+++ Practice/DemoApp/RealTimePlot/obj/Debug/net8.0/RealTimePlot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealTimePlot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d9d432ab8d057da20d25e9ec3b6749cda222af4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e749e8dd62fdd2662004d5d53e46a51a09c2d963")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealTimePlot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealTimePlot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
