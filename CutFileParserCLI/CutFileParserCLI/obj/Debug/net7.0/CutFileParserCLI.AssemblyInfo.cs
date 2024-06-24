26ba327bdacd2494509bae6f15a2e7edb462b0c8
Yaolin Ge <geyaolin@gmail.com>
2024-06-24 16:13:53 +0200

    Update CutFileParserCLI

--- CutFileParserCLI/CutFileParserCLI/obj/Debug/net7.0/CutFileParserCLI.AssemblyInfo.cs
+++ CutFileParserCLI/CutFileParserCLI/obj/Debug/net7.0/CutFileParserCLI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CutFileParserCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d9d432ab8d057da20d25e9ec3b6749cda222af4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5588475769064162328400587a0bac78256cb019")]
 [assembly: System.Reflection.AssemblyProductAttribute("CutFileParserCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CutFileParserCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
