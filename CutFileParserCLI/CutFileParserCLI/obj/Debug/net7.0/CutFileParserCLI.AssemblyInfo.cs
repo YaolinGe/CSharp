85d0dd661d0df79199f2c462d8cb63abb504c7ce
Yaolin Ge <geyaolin@gmail.com>
2024-06-24 18:02:08 +0200

    Update the sensor list

--- CutFileParserCLI/CutFileParserCLI/obj/Debug/net7.0/CutFileParserCLI.AssemblyInfo.cs
+++ CutFileParserCLI/CutFileParserCLI/obj/Debug/net7.0/CutFileParserCLI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CutFileParserCLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5588475769064162328400587a0bac78256cb019")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ba327bdacd2494509bae6f15a2e7edb462b0c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CutFileParserCLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CutFileParserCLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
